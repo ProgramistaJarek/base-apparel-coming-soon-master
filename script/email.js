c3394892ca42162dd0fc7d3bc4ccf8d863aebd25
Jarosław Lepich <dojarka0@gmail.com>
2020-12-18 20:52:04 +0100

    done with validation, very nice project to do

--- script/email.js
+++ script/email.js
@@ -1,8 +1,27 @@
 window.onload = function () {
   let email = document.querySelector('input[name="email"]');
-  let btn = document.querySelector("button");
+  const btn = document.querySelector("button");
+  let infoEmail = document.querySelector(".info");
+  let errorEmail = document.querySelector(".error");
+  let formErrorEmail = document.querySelector("form");
 
   btn.addEventListener("click", () => {
-    console.log(email.value);
+    if (!ValidateEmail(email)) {
+      infoEmail.classList.add("active");
+      errorEmail.classList.add("active");
+      formErrorEmail.classList.add("formErrorEmail");
+    } else {
+      infoEmail.classList.remove("active");
+      errorEmail.classList.remove("active");
+      formErrorEmail.classList.remove("formErrorEmail");
+    }
   });
+  function ValidateEmail(inputText) {
+    let mailformat = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (inputText.value.match(mailformat)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
